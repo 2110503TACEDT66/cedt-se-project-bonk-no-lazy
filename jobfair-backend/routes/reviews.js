2bce75e98702fa0cc3beeebb84e80190c17e4f3a
Kittipong <kittipong@gmail.com>
2024-04-10 12:23:26 +0700

    edit model commit

--- jobfair-backend/routes/reviews.js
+++ jobfair-backend/routes/reviews.js
@@ -5,7 +5,7 @@ const {protect,authorize} = require('../middleware/auth');
 
 const router = express.Router({mergeParams:true});
 
-router.route('/').get(protect,getReviews).post(protect,authorize('admin','user'),addReview);
-router.route('/:id').get(protect,getReview).put(protect,authorize('admin','user'),updateReview).delete(protect,authorize('admin','user'),deleteReview);
+router.route('/').get(getReviews).post(protect,authorize('admin','user'),addReview);
+router.route('/:id').get(getReview).put(protect,authorize('admin','user'),updateReview).delete(protect,authorize('admin','user'),deleteReview);
 
 module.exports = router;
