5b973f201f477e2331b2408831ba78cfd8023330
imsobluu <11052192+imsobluu@users.noreply.github.com>
2024-04-18 22:04:36 +0700

    cleaned up code

--- jobfair-frontend/src/components/TopMenu.tsx
+++ jobfair-frontend/src/components/TopMenu.tsx
@@ -24,39 +24,45 @@ export default async function TopMenu(){
         <div className='h-[50px] bg-neutral-900 border-neutral-700 border-t-0 border-l-0 border-r-0 border-2 fixed top-0 right-0 z-30 flex flex-row
         rounded-b-lg w-[100vw] items-center pl-[10px] pr-[50px]'>
             <Link href='/' >
-                    <div className='flex items-center h-[70%] px-2 py-2 rounded-lg 
-                     text-black bg-white text-sm
-                     flex-start top-2 mx-2 transition transform duration-200 ease hover:scale-105' >
-                        Home
-                    </div>
-                </Link>
+                <div
+                    className='flex items-center h-[70%] px-2 py-2 rounded-lg 
+                    text-black bg-white text-sm
+                    flex-start top-2 mx-2 transition transform duration-200 ease hover:scale-105' >
+                    Home
+                </div>
+            </Link>
             {
                 session?
                 <>
                     <Link href='/api/auth/signout'>
-                        <div className='flex items-center h-[70%] px-2 py-2 rounded-lg 
-                        text-black bg-white text-sm
-                        left-10 top-2 transition transform duration-200 ease hover:scale-105'>
+                        <div
+                            className='flex items-center h-[70%] px-2 py-2 rounded-lg 
+                            text-black bg-white text-sm
+                            left-10 top-2 mr-2 transition transform duration-200 ease hover:scale-105'>
                             Sign Out of {profile?.data.name}
                         </div>
                     </Link>
-                    <Link href='/mybooking' >
-                        <div className='flex items-center h-[70%] px-2 py-2 rounded-lg 
+                    <Link href='/mybooking'>
+                        <div
+                            className='flex items-center h-[70%] px-2 py-2 rounded-lg 
                             text-black bg-white text-sm
-                            flex-start top-2 mx-2 transition transform duration-200 ease hover:scale-105'>
-                                My Booking
+                            flex-start top-2 mr-2 transition transform duration-200 ease hover:scale-105'>
+                            My Booking
                         </div>
                     </Link>
                     {
-                        profile?.data.role == 'company'?<Link href='/mycompany' >
-                        <div id='sigin-button' className='flex items-center h-[70%] px-2 py-2 rounded-lg 
-                        text-black bg-white text-sm
-                        flex-start top-2 mx-2 transition transform duration-200 ease hover:scale-105'>
-                            My Company Profile
-                        </div>
-                    </Link>:''
+                        profile?.data.role == 'company'?
+                            <Link href='/mycompany'>
+                                <div
+                                    className='flex items-center h-[70%] px-2 py-2 rounded-lg 
+                                    text-black bg-white text-sm
+                                    flex-start top-2 mr-2 transition transform duration-200 ease hover:scale-105'>
+                                    My Company Profile
+                                </div>
+                            </Link>
+                            :
+                            ''
                     }
-                    
                     <Link href='/booking'>
                         <div className='absolute right-20 h-[70%] px-2 py-2 rounded-lg 
                             text-black bg-white text-sm
@@ -65,7 +71,8 @@ export default async function TopMenu(){
                         </div>
                     </Link>
                 </>
-                :(
+                :
+                (
                     <> 
                         <Link href='api/auth/signin' >
                             <div id='sigin-button' className='flex items-center h-[70%] px-2 py-2 rounded-lg 
