da8fa632f6ac0ee19200874e767dde55fc37f5d5
imsobluu <11052192+imsobluu@users.noreply.github.com>
2024-04-21 21:40:45 +0700

    new navbar added

--- jobfair-frontend/src/app/page.tsx
+++ jobfair-frontend/src/app/page.tsx
@@ -1,26 +1,7 @@
-import Banner from "@/components/Banner";
-import PromoteCard from "@/components/PromoteCard";
-import { getServerSession } from "next-auth";
-import getUserProfile from "@/libs/getUserProfile";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-
 export default async function Home() {
-
-  const session = await getServerSession(authOptions)
-
-    var profile;
-
-    if (session) {
-        profile = await getUserProfile(session.user.token)
-    }
-
   return (
-    <main>
-      {
-        session?
-        <Banner user={profile}/> :
-        <Banner/>
-      }
-    </main>
+    <div className="text-rose-500 text-2xl">
+      Hello World
+    </div>
   );
 }
