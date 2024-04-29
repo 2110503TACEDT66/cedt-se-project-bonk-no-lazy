be26cc349253b993008fd7ea29fcc68c9bd4a352
imsobluu <11052192+imsobluu@users.noreply.github.com>
2024-04-29 10:23:33 +0700

    removed old files

--- libsOLD/getReviews.tsx
+++
@@ -1,9 +0,0 @@
-export default async function getReviews() {
-
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/reviews`)
-    if(!response.ok){
-        throw new Error('Failed to fetch reviews')
-    }
-    return await response.json();
-    
-}
