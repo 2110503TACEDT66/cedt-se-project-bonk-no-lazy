9a6ec03310a33d26795826bea64488db3691add4
imsobluu <11052192+imsobluu@users.noreply.github.com>
2024-05-01 01:02:01 +0700

    added shadow on hover

--- jobfair-frontend/src/components/jobPositions/JobPositionCard.tsx
+++ jobfair-frontend/src/components/jobPositions/JobPositionCard.tsx
@@ -66,9 +66,9 @@ const JobPositionCard: React.FC<JobPositionCardProps> = ({
         transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
         overflow: "hidden",
         position: "relative",
-        boxShadow: "rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px",
         borderRadius: "16px",
       }}
+      className="shadow-md hover:shadow-xl cursor-pointer"
     >
       <div 
         className="
