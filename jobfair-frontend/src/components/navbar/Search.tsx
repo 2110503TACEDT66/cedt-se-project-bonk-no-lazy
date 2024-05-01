aef0b7c407abd4264f7c15bc542d84e491b31000
imsobluu <11052192+imsobluu@users.noreply.github.com>
2024-05-01 08:28:02 +0700

    remove search

--- jobfair-frontend/src/components/navbar/Search.tsx
+++ jobfair-frontend/src/components/navbar/Search.tsx
@@ -49,67 +49,13 @@ const Search = () => {
                         text-m
                         font-bold
                         px-6
-                        border-x-[1px]
+                        border-l-[1px]
                         flex-1
                         text-center
                         hover:text-blue-500 cursor-pointer
                     "
           >
             <MenuItem label="Jobs" onClick={() => router.push("/jobs")} />
-          </div>
-          <div
-            className="
-                        text-m
-                        pl-2
-                        pr-5
-                        text-gray-400
-                        flex
-                        flex-row
-                        items-center
-                        gap-3
-                        
-                    "
-          >
-            <div
-              className="
-                            hidden
-                            sm:block
-                        "
-            >
-            </div>
-            <div className="container">
-              <input
-                type="text"
-                name="text"
-                className="input"
-                required
-                placeholder="Type to search..."
-              />
-              <div className="icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="ionicon"
-                  viewBox="0 0 512 512"
-                >
-                  <title>Search</title>
-                  <path
-                    d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeMiterlimit="10"
-                    strokeWidth="32"
-                  ></path>
-                  <path
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeMiterlimit="10"
-                    strokeWidth="32"
-                    d="M338.29 338.29L448 448"
-                  ></path>
-                </svg>
-              </div>
-            </div>
           </div>
         </div>
       </div>
